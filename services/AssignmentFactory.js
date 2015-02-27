b0339b46ab9f959e48d6f7671df463d64329882f
Gabe <gabefinch@gmail.com>
2015-02-27 11:16:59 -0800

    Add reporters within assignment objects and li within li

--- services/AssignmentFactory.js
+++ services/AssignmentFactory.js
@@ -2,10 +2,24 @@ reporterAssignments.factory('AssignmentFactory',
   function AssignmentFactory() {
   var factory = {};
 
-  factory.assignments = [ {name: "UFO sightings", id: 1, reporters: [] },
-                    {name: "Crime wire", id: 2, reporters: [] },
-                    {name: "National Government", id: 3, reporters: [] },
-
+// ASSIGNMENTS
+  factory.assignments = [
+    {name: "UFO sightings", id: 1, reporters: [
+      {name: "Bob Bobson", id: 1 , yearsWorked: 15},
+      {name: "Claire Clarice", id: 2 , yearsWorked: 3},
+      {name: "Sammy Samson", id: 3 , yearsWorked: 6}
+      ]
+    },
+    {name: "Crime wire", id: 2, reporters: [
+      {name: "Bob Bobson", id: 1 , yearsWorked: 15},
+      {name: "Claire Clarice", id: 2 , yearsWorked: 3}
+      ]
+    },
+    {name: "National Government", id: 3, reporters: [
+      {name: "Claire Clarice", id: 2 , yearsWorked: 3},
+      {name: "Sammy Samson", id: 3 , yearsWorked: 6}
+      ]
+    },
   ];
 
   factory.addAssignment = function(name) {
@@ -13,7 +27,12 @@ reporterAssignments.factory('AssignmentFactory',
       id: factory.assignments.length + 1 });
   };
 
+// REPORTERS
 
+  factory.addReporter = function() {
+    factory.assignments.push({ name: name, reporters: [],
+      id: factory.assignments.length + 1 });
+  };
 
   return factory;
 });
