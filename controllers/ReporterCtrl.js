924eded541f58e80cf7a702ad4424962edbdee09
Gabe <gabefinch@gmail.com>
2015-02-27 16:08:04 -0800

    Fully functional add reporter

--- controllers/ReporterCtrl.js
+++ controllers/ReporterCtrl.js
@@ -1,14 +1,13 @@
 reporterAssignments.controller('ReporterCtrl',
 function ReporterCtrl($scope, $state, $stateParams, AssignmentFactory, UtilityFactory) {
 
-
-  $scope.hello = 'hello kitty'
   $scope.AssignmentFactory = AssignmentFactory;
   $scope.assignment = UtilityFactory.findById(
     AssignmentFactory.assignments, $stateParams.assignmentId);
 
-  $scope.addReporterThen = function() {
-    debugger;
+  $scope.addReporterThen = function(name, years, bday) {
+    AssignmentFactory.addReporter( $scope.assignment, name, years, bday);
+    $state.go('home');
   };
 
 });
